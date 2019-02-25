f3ac22050be2320a9f1334f32f0e2a4040b2fb21
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2019-02-25 18:26:48 +0800

    Solved degree array problem.

--- src/algorithmic_heights/mod.rs
+++ src/algorithmic_heights/mod.rs
@@ -1,5 +1,7 @@
-use std::cmp::Ord;
+use std::cmp::{Eq, Ord};
+use std::hash::Hash;
 use std::marker::Copy;
+use std::collections::{HashMap, HashSet};
 
 pub fn fibo(n: u32) -> u32 {
 	let mut i = 0;
@@ -12,7 +14,7 @@ pub fn fibo(n: u32) -> u32 {
 	i
 }
 
-fn binary_search<T: Ord + Copy>(sorted_arr: &Vec<T>, n: T) -> i32 {
+fn binary_search<T: Ord + Copy>(sorted_arr: &[T], n: T) -> i32 {
 	let mut i = 0;
 	let mut j = sorted_arr.len()- 1;
 	let mut m = 0;
@@ -32,8 +34,7 @@ fn binary_search<T: Ord + Copy>(sorted_arr: &Vec<T>, n: T) -> i32 {
 	loop {
 		let tmp = (i + j) / 2;
 		if tmp == m {
-			// not found
-			return -1;
+			return -1; // not found
 		}
 		m = tmp;
 
@@ -45,14 +46,53 @@ fn binary_search<T: Ord + Copy>(sorted_arr: &Vec<T>, n: T) -> i32 {
 		} else if val < n {
 			i = m;
 		}
-
 	}
 }
 
-pub fn bins<T: Ord + Copy>(sorted_arr: &Vec<T>, n: T) -> i32 {
+pub fn bins<T: Ord + Copy>(sorted_arr: &[T], n: T) -> i32 {
 	// add one to get 1 based index
 	match binary_search(sorted_arr, n) {
 		-1 => -1,	// preserve -1
 		x  => x + 1,
 	}
+}
+
+pub fn deg<T: Eq + Hash + Ord + Copy>(input_arr: &[Vec<T>]) -> Vec<usize> {
+	let mut graph: HashMap<T, HashSet<T>> = HashMap::with_capacity(input_arr.len());
+	for edge in input_arr {
+		let a = edge[0];
+		let b = edge[1];
+
+		if graph.contains_key(&a) {
+			if let Some(set) = graph.get_mut(&a) {
+				set.insert(b);
+			};
+		} else {
+			let mut set = HashSet::new();
+			set.insert(b);
+			graph.insert(a, set);
+		}
+
+		if graph.contains_key(&b) {
+			if let Some(set) = graph.get_mut(&b) {
+				set.insert(a);
+			};
+		} else {
+			let mut set = HashSet::new();
+			set.insert(a);
+			graph.insert(b, set);
+		}
+	}
+
+	let mut sorted_idx = vec![];
+	for (key, _) in &graph {
+		sorted_idx.push(*key);
+	}
+	sorted_idx.sort();
+
+	let result = sorted_idx
+					.into_iter()
+					.map(|k| graph[&k].len())
+					.collect();
+	return result;
 }
---
+++ src/bin/deg.rs
@@ -0,0 +1,18 @@
+extern crate rosalind;
+use rosalind::{algorithmic_heights, utility::parser};
+
+fn parse_input() -> Vec<Vec<u32>> {
+	let filename = format!("input_dataset/{}", &parser::cmdline_arguments()[1]);
+	return parser::list_of_things(filename).unwrap();
+}
+
+fn main() {
+	let input_arr = parse_input();
+	let res = algorithmic_heights::deg(&input_arr[1..])
+				.into_iter()
+				.map(|x| x.to_string())
+				.collect::<Vec<String>>()
+				.join(" ");
+
+	println!("{}", res);
+}
