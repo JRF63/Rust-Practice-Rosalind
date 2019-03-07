7184cb5a2020a412ecc8e6e6157be7f9e1191b86
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2019-03-07 08:43:22 +0800

    Finished breadth first search.

--- src/algorithmic_heights/graph.rs
+++ src/algorithmic_heights/graph.rs
@@ -1,7 +1,7 @@
 use std::hash::Hash;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
-fn create_graph<T: Eq + Hash + Ord + Copy>(edge_list: &[Vec<T>]) -> HashMap<T, HashSet<T>> {
+pub fn create_graph<T: Eq + Hash + Ord + Copy>(edge_list: &[Vec<T>]) -> HashMap<T, HashSet<T>> {
 	let mut graph: HashMap<T, HashSet<T>> = HashMap::with_capacity(edge_list.len());
 	for edge in edge_list {
 		let a = edge[0];
@@ -9,6 +9,17 @@ fn create_graph<T: Eq + Hash + Ord + Copy>(edge_list: &[Vec<T>]) -> HashMap<T, H
 
 		graph.entry(a).or_insert(HashSet::new()).insert(b);
 		graph.entry(b).or_insert(HashSet::new()).insert(a);
+	}
+	return graph;
+}
+
+pub fn create_digraph<T: Eq + Hash + Ord + Copy>(edge_list: &[Vec<T>]) -> HashMap<T, HashSet<T>> {
+	let mut graph: HashMap<T, HashSet<T>> = HashMap::with_capacity(edge_list.len());
+	for edge in edge_list {
+		let a = edge[0];
+		let b = edge[1];
+
+		graph.entry(a).or_insert(HashSet::new()).insert(b);
 	}
 	return graph;
 }
@@ -44,5 +55,32 @@ pub fn ddeg(input_arr: &[Vec<u32>]) -> Vec<usize> {
 			None => result.push(0),
 		}
 	}
+	return result;
+}
+
+pub fn bfs(graph: &HashMap<usize, HashSet<usize>>, num_nodes: usize) -> Vec<i32> {
+	let mut result: Vec<i32> = std::iter::repeat(-1).take(num_nodes).collect();
+
+	let mut queue = VecDeque::new();
+	let mut visited = HashSet::new();
+
+	let start = 1;
+	queue.push_back((start, 0));
+	visited.insert(start);
+
+	while !queue.is_empty() {
+		let (node, dist) = queue.pop_front().unwrap();
+		result[node - 1] = dist;
+
+		if let Some(set) = graph.get(&node) {
+			for next in set {
+				if !visited.contains(next) {
+					visited.insert(*next);
+					queue.push_back((*next, dist + 1));
+				}
+			}
+		}
+	}
+
 	return result;
 }
